be38d00dde449b09935c3ed8f21bd120ed1379b6
saulova <9355603+saulova@users.noreply.github.com>
2024-12-13 15:43:02 -0300

    docs: add url config

--- .docs/astro.config.mjs
+++ .docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://saulova.github.io",
+  base: "seam",
   integrations: [
     starlight({
       title: "🪡 Seam API Gateway",
